2c232a53fdc54f4baf7a50118828c096a51ecdd1
jacob f <jacob@jacobs-MBP.attlocal.net>
2020-03-16 13:13:46 -0700

    completed drill 1

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 'use strict';
 
-function yearOfBirth(age) {
+function getYearOfBirth(age) {
     if (age < 0) {
         throw new Error('Age can not be negative');
-    } else {const yob = 2020 - age;
+    } else { const yob = 2020 - age;
     return yob;
     }
 }
 
 function createGreeting(name, age) {
-    return `Hi my name is ${name}, I\'m ${age} years old, and I was born in year ${yearOfBirth}`;
+    if (name === undefined || age === undefined ) {
+        throw new Error ('Arguments not valid');
+    } 
+    if (typeof name !== 'string' || typeof age !== 'number') {
+        throw new TypeError()
+    }
+    const yob = getYearOfBirth(age);
+    return `Hi my name is ${name}, I\'m ${age} years old, and I was born in year ${yob}`;
 }
 
 try {
-    const greeting1 = createGreeting();
+    const greeting1 = createGreeting('Jacob', 22);
     console.log(greeting1);
-} catch (Error) {
-    console.log(Error);
+} catch(e) {
+    console.error(e.message);
 };
