7e44fca7e60dcc87466182041bc03a6eeea5b73f
jacob f <jacob@jacobs-MBP.attlocal.net>
2020-03-16 12:41:10 -0700

    added new function with year of birth

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
 
+function yearOfBirth(age) {
+    const yob = 2020 - age;
+    return yob;
+}
+
 function createGreeting(name, age) {
-    return `Hi my name is ${name} and I\'m ${age} years old`;
+    return `Hi my name is ${name}, I\'m ${age} years old, and I was born in year ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
