e3ef0c814a566c3bc06ae3c2e1b7db58710cc453
Kevin Jiang <kjiang95@gmail.com>
2020-03-16 14:34:37 -0500

    changed js function to return the function

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function createGreeting() {
-    console.log(`Hi my name is Kevin and I\'m 24 years old`);
+function createGreeting(name, age) {
+    return `Hi my name is ${name} and I\'m ${age} years old`;
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
