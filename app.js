90b57ab6a14ef3518a8ddae06d36f8b44c75843d
Kevin Jiang <kjiang95@gmail.com>
2020-03-16 14:51:43 -0500

    changed js code

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 'use strict';
 
 function yearOfBirth(age) {
-    const yob = 2020 - age;
+    if (age < 0) {
+        throw new Error('Age can not be negative');
+    } else {const yob = 2020 - age;
     return yob;
+    }
 }
 
 function createGreeting(name, age) {
     return `Hi my name is ${name}, I\'m ${age} years old, and I was born in year ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
-
+try {
+    const greeting1 = createGreeting();
+    console.log(greeting1);
+} catch (Error) {
+    console.log(Error);
+};
